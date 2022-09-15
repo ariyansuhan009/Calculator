538d1c6573d9f94c64207c146118b7f75eeb6fbc
ariyansuhan009 <ariyansuhan009@gmail.com>
2022-09-15 16:17:00 +0000

    first commit

---
+++ src/main/java/com/example/calculator/Main.java
@@ -0,0 +1,9 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public abstract class Main extends Application{
+}
